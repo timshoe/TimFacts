9f2b9c4ccabd1fa7a54f7bbb9f172aa8c6b72c8e
timshoe <tim.xu67@gmail.com>
2017-05-30 13:45:29 -0400

    testing commits and adding photo files

---
+++ FunFacts2/FactModel.swift
@@ -0,0 +1,9 @@
+//
+//  FactModel.swift
+//  FunFacts
+//
+//  Created by Tim Xu on 5/20/17.
+//  Copyright © 2017 TimXu. All rights reserved.
+//
+
+import Foundation
---
+++ FunFacts2/PictureModel.swift
@@ -0,0 +1,9 @@
+//
+//  PictureModel.swift
+//  TimFacts
+//
+//  Created by Tim Xu on 5/21/17.
+//  Copyright © 2017 TimXu. All rights reserved.
+//
+
+import Foundation
